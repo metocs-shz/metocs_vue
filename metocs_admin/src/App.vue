45057fb321d38d033ff6801e5dc77d4d4eba48a8
metocs <164857362@qq.com>
2024-01-20 14:06:35 +0800

    提交Hello Word程序

---
+++ metocs_admin/src/App.vue
@@ -0,0 +1,32 @@
+
+
+
+
+
+<template>
+  <div class="hello">
+    <h1>Hello World Vue !</h1>
+  </div>
+
+</template>
+
+
+<script lang="ts">
+export default {
+  name: 'app'
+}
+
+</script>
+
+<style scoped>
+
+.hello{
+  background-color: aquamarine;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+
+}
+
+
+</style>
